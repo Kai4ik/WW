d5844bb2cf6f2d46623d0cac8b2c23ccf368aa89
Kairat Orozobekov <orozobekov.kai@gmail.com>
2021-03-22 10:50:40 -0400

    WW ready

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import $ from "jquery";
 import styles from "./navigation.module.css";
 import { HashLink } from "react-router-hash-link";
